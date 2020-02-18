8e2ef23feea84a16b5309629fb6439f3d445104a
Rory O'Neill <rory_oneill_1989@hotmail.com>
2020-02-18 12:27:11 +0000

    view all working as expected

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CREATE
+  # AS A USER I CAN ADD A NEW TASK
+  # READ
+  # AS A USER I CAN LIST ALL TASKS
+  get 'tasks', to: 'tasks#index'
+  # AS A USER I CAN VIEW THE DETAILS OF A SPECIFIC TASK
+  # UPDATE
+  # AS A USER I CAN EDIT A TASK
+  # DELETE
+  # AS A USER I CAN REMOVE A TASK
 end
