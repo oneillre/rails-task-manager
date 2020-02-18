b93b88cb96aaa8df362304896c842d9d268ed77b
Rory O'Neill <rory_oneill_1989@hotmail.com>
2020-02-18 13:16:35 +0000

    user can now add tasks

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # CREATE
   # AS A USER I CAN ADD A NEW TASK
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
   # READ
   # AS A USER I CAN LIST ALL TASKS
   get 'tasks', to: 'tasks#index'
   # AS A USER I CAN VIEW THE DETAILS OF A SPECIFIC TASK
+  get 'tasks/:id', to: 'tasks#show', as: :task
   # UPDATE
   # AS A USER I CAN EDIT A TASK
   # DELETE
